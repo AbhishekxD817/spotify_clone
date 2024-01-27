72c1657659103a7061ab40e18c9a2bcb716cca20
Abhishek Kumar <ab2004ak@gmail.com>
2024-01-27 17:21:40 +0530

    responsiveness added

--- scripts/app.js
+++ scripts/app.js
@@ -15,3 +15,44 @@ cards.forEach((card)=>{
         playButton.classList.toggle("hide")
     })
 })
+
+
+
+
+// let songs_url = "http://127.0.0.1:5500/songs/"
+// let songs = []
+
+// async function getSongs(){
+//     let a = await fetch(songs_url)
+//     console.log(a)
+//     a = await a.text()
+//     console.log(a)
+
+//     let div = document.createElement("div")
+//     div.innerHTML = a
+
+//     let as = div.querySelectorAll("a")
+//     console.log(as)
+
+    
+//     for (const a of as) {
+//         let href = a.getAttribute('href')
+//         if(href.endsWith(".mp3")){
+//             console.log(href)
+//             songs.push(href)
+//         }
+//     }
+
+//     console.log(songs)
+    
+// }
+// getSongs()
+
+
+
+// let body = document.querySelector("body")
+// body.innerHTML="<audio controls autoplay muted>
+// <source src=horse.ogg type='audio/mp3'>
+// <source src="horse.mp3" type="audio/mpeg">
+// Your browser does not support the audio element.
+// </audio>"
